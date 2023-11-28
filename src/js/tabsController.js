0d684240a9bb0d4bd8377e41cac904bdc173ca9b
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2023-11-28 15:23:49 +0300

    feat: section home - html, css done

--- src/js/tabsController.js
+++ src/js/tabsController.js
@@ -116,9 +116,70 @@ function renderChart() {
 
 function renderHome() {
   const homeHTML = `
-    <div class="home development-section" id='home' data-active="active-tab">
-      <h3>Home in development ...</h3>
-    </div>
+    <div class="home" data-active="active-tab">
+          <div class="home__container">
+            <div class="home__content">
+              <div class="data">
+                <div class="data__block">
+                  <h3 class="data__title">Profit avg in mounth</h3>
+                  <div class="data__wrapper">
+                    <div class="data__value">8933.00 <span>USD</span></div>
+                  </div>
+                </div>
+                <div class="data__block">
+                  <h3 class="data__title">Expense avg in mounth</h3>
+                  <div class="data__wrapper">
+                    <div class="data__value">3009.15 <span>USD</span></div>
+                  </div>
+                  <div class="balance">
+                    <div class="balance__info">Account balance: <span>960.00</span> USD</div>
+                    <div class="balance__info">Current expense: <span>2001.00</span> USD</div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="statistics">
+                <div class="statistics-header">
+                  <h1 class="statistics__title">Statistics All</h1>
+                  <select class='statistics__select' name="time" id="time">
+                    <option class='statistics__option' value="" disabled selected hidden>Choose Time</option>
+                    <option class='statistics__option' value="day">day</option>
+                    <option class='statistics__option' value="week">week</option>
+                    <option class='statistics__option' value="mounth">mounth</option>
+                    <option class='statistics__option' value="year">year</option>
+                    <option class='statistics__option' value="all">all</option>
+                  </select>
+                </div>
+                <div class="chart">
+                  <div class="chart__list">
+                    <div class="chart__el">
+                      <h3 class="chart__title">Product <span class='chart_sum'>1200.00</span></h3>
+                      <div class="chart__linear chart__linear-product"><span class='percent'>30%</span></div>
+                    </div>
+                    <div class="chart__el">
+                      <h3 class="chart__title">Home <span class='chart_sum'>700.00</span></h3>
+                      <div class="chart__linear chart__linear-home"><span class='percent'>20%</span></div>
+                    </div>
+                    <div class="chart__el">
+                      <h3 class="chart__title">Fun <span class='chart_sum'>400.00</span></h3>
+                      <div class="chart__linear chart__linear-fun"><span class='percent'>15%</span></div>
+                    </div>
+                    <div class="chart__el">
+                      <h3 class="chart__title">Other <span class='chart_sum'>1500.00</span></h3>
+                      <div class="chart__linear chart__linear-other"><span class='percent'>35%</span></div>
+                    </div>
+                  </div>
+
+                  <div class="chart__sum">
+                    <span class="chart__sum-title">Total expenses</span>
+                    <span class="chart__sum-number">2990.00</span>
+                    <span class="chart__sum-currency">USD</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
   `
   mainContainer.insertAdjacentHTML('afterbegin', homeHTML)
 }
